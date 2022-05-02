b78e20555edab64346ab38d06efa76246788df8c
Manoj Kumar <manojkumar024@gmail.com>
2022-05-02 22:44:54 +0530

    Update Githubaction-script.sh
    sixth commit

--- Githubaction-script.sh
+++ Githubaction-script.sh
@@ -1,7 +1,26 @@
-MAX=95
-EMAIL=server@127.0.0.1
-
-USE=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}')
-if [ $USE -gt $MAX ]; then
-	echo "Percent used: $USE" | mail -s "Running out of CPU power" $EMAIL
+#!/bin/bash
+MAX_NO=0
+echo -n "Enter Number between (5 to 9) : "
+read MAX_NO
+if ! [ $MAX_NO -ge 5 -a $MAX_NO -le 9 ] ; then
+   echo "WTF... I ask to enter number between 5 and 9, Try Again"
+   exit 1
 fi
+clear
+for (( i=1; i<=MAX_NO; i++ )) do     for (( s=MAX_NO; s>=i; s-- ))
+    do
+       echo -n " "
+    done
+    for (( j=1; j<=i;  j++ ))     do      echo -n " ."      done     echo "" done ###### Second stage ###################### for (( i=MAX_NO; i>=1; i-- ))
+do
+    for (( s=i; s<=MAX_NO; s++ ))
+    do
+       echo -n " "
+    done
+    for (( j=1; j<=i;  j++ ))
+    do
+     echo -n " ."
+    done
+    echo ""
+done
+echo -e "\n\n\t\t\t Whenever you need help, Tecmint.com is always there"
